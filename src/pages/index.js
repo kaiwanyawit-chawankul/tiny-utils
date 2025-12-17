ccafa69ba2482bd692c9cb130e3c73365677b5c7
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-12-17 21:43:15 +0700

    require login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 // pages/index.js
 import Navbar from '@components/Navbar';
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
+  const { user, isLoading } = useUser();
+
   return (
     <>
       <Navbar />
@@ -11,12 +14,27 @@ export default function Home() {
         <p>Securely share text or shorten links, all in one place.</p>
 
         <div style={{ marginTop: '2rem' }}>
-          <Link href="/paste/new">
-            <button style={buttonStyle}>Create Paste</button>
-          </Link>
-          <Link href="/shorten">
-            <button style={buttonStyle}>Shorten URL</button>
-          </Link>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : user ? (
+            <>
+              <Link href="/paste/new">
+                <button style={buttonStyle}>Create Paste</button>
+              </Link>
+              <Link href="/shorten">
+                <button style={buttonStyle}>Shorten URL</button>
+              </Link>
+            </>
+          ) : (
+            <div>
+              <p style={{ marginBottom: '1rem', fontSize: '1.1rem' }}>
+                Please log in to access these features.
+              </p>
+              <Link href="/auth/login">
+                <button style={buttonStyle}>Login</button>
+              </Link>
+            </div>
+          )}
         </div>
       </main>
     </>
